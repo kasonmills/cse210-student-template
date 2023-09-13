b5a144177c6421cbe630e4fa8dbc8844100433e9
KasonMills <kmillsventura@byui.edu>
2023-09-13 14:54:15 -0600

    finished assingments

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        
+        
+        Console.WriteLine("What is your grade percentage?");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+
+        string letter_grade = "A";
+
+        if(grade >= 90)
+        {
+            letter_grade = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter_grade = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter_grade = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter_grade = "D";
+        }
+        else
+        {
+            letter_grade = "F";
+        }
+        
+        Console.WriteLine($"your letter grade is {letter_grade}");
+
+        if(letter_grade == "A"|| letter_grade == "B" || letter_grade == "C")
+        {
+            Console.WriteLine("Congrats you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time.");
+        }
+
     }
 }
