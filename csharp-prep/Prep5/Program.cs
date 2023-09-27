f0d2843125e5875e4acf6be1a87cd954441fa78f
KasonMills <kmillsventura@byui.edu>
2023-09-27 10:50:23 -0600

    adding classes for develop02

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -5,5 +5,41 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int number2 = SquareNumber(number);
+        DisplayResult(number2, name);
+    }
+
+    static public void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.WriteLine("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("Please enter your favorite number");
+        string prompt = Console.ReadLine();
+        int number = int.Parse(prompt);
+        return number;
+    }
+
+    static int SquareNumber(int number)
+    {
+        int number2 = number * number;
+        return number2;
+    }
+
+    static void DisplayResult(int number2, string name)
+    {
+        Console.WriteLine($"Brother {name}, the square of your number is {number2}");
     }
 }
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,5 +5,37 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+
+        var car1 = new Car();
+        car1.make = "Honda";
+        car1.model = "Civic";
+        car1.mileage = 30;
+        car1.gallonsOfFuel = 10;
+
+        var miles = car1.RemainingMiles;
+
+        car1.ReduceFuel(3);
+
+        //string carMake1 = "Honda";
+        //string carModel1 = "Civic";
+        //int carMileage1 = 33;
+    }
+}
+
+public class Car
+{
+    public string make;
+    public string model;
+    public int mileage;
+    public int gallonsOfFuel;
+
+    public int RemainingMiles()
+    {
+        return gallonsOfFuel * mileage;
+    }
+
+    public void ReduceFuel(int amount)
+    {
+        gallonsOfFuel = gallonsOfFuel - amount;
     }
 }
