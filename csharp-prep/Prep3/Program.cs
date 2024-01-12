062499c5d7d0004f93f71741346a7ef951e1dd6a
KasonMills <kmillsventura@byui.edu>
2024-01-12 16:23:30 -0700

    moar work

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,65 @@
 using System;
+using System.Dynamic;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
+        Random Rd = new Random();
         Console.WriteLine("Hello Prep3 World!");
+        /*
+        Console.WriteLine("What is the Magic number? ");
+        string cin = Console.ReadLine();
+        int magic = int.Parse(cin);
+        */
+        Console.WriteLine("Welcome to the guessing game!");
+        bool again = true;
+        
+        do
+        {
+            int guess = GetGuess();
+            int magic = Rd.Next(1,100);
+            int attempts = 0;        
+
+            while (guess != magic)
+            {
+                if (guess > magic)
+                {
+                    Console.WriteLine("Lower");
+                    guess = GetGuess();
+                    attempts++;
+                }
+                else if (guess < magic)
+                {
+                    Console.WriteLine("Higher");
+                    guess = GetGuess();
+                    attempts++;
+                }
+                else
+                {
+                    break;
+                }
+                Console.WriteLine("You guessed it!");
+                Console.WriteLine($"It took you {attempts} guesses");
+            }
+
+            Console.WriteLine("Would you like to play again? (please input y or no)");
+            string confirm = Console.ReadLine();
+
+            if (confirm == "n")
+            {
+                break;
+            }
+
+        }while (again == true);
+    }
+
+    static int GetGuess()
+    {
+        Console.WriteLine("What is your guess? ");
+        string c2 = Console.ReadLine();
+        int guess = int.Parse(c2);
+        return guess;
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,5 +5,38 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished");
+        List<int> numbers = new List<int>();
+        int num = GetNumber();
+
+        while (num != 0)
+        {
+            num = GetNumber();
+            numbers.Add(num);
+        }
+
+        int cou = numbers.Count();
+        int sum = 0;
+
+        for (int a = 0; a < cou; a++)
+        {
+            sum += numbers[a];
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        double avg = sum / cou;
+        Console.WriteLine($"The average is: {avg}");
+
+        int max = numbers.Max();
+        Console.WriteLine($"The largest number is: {max}");
+    }
+
+    static int GetNumber()
+    {
+        Console.WriteLine("Enter Number: ");
+        string item = Console.ReadLine();
+        int num = int.Parse(item);
+        return num;
     }
 }
