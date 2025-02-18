0e6e262705776699e7e093b47617b4afb88c7c7f
KasonMills <kmillsventura@byui.edu>
2025-02-18 08:17:27 -0700

    fixing old issues.

---
+++ prove/Develop02/journal.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+
+namespace journal;
+public class Journal
+{
+
+    // program class that interacts with journal- journal the backend to let entry and prompt generator run.
+    PromptGenerator _prompt = new PromptGenerator();
+    Entry _entry = new Entry();
+    List<string> _savedEntries = new List<string>();
+    List<string> _newEntry = new List<string>();
+
+
+    public void SelectOption(int _option)
+    {
+        switch (_option)
+        {
+            case 1:
+                Console.WriteLine(_entry.Get_date());
+                Console.WriteLine(_prompt.Get_prompt());
+                _newEntry =_entry.Create_entry(_entry.Get_date(),_prompt.Get_prompt());
+                break;
+
+            case 2:
+                _entry.Display_text(_newEntry);
+                break;
+            case 3:
+                _entry.Save_text(Get_file_name(), _newEntry);
+                break;
+            case 4:
+                _entry.Read_file(Get_file_name(), _savedEntries);
+                _entry.Display_text(_savedEntries);
+                break;
+            default:
+                break;
+        }
+
+    }
+    public static string Get_file_name()
+    {
+        /*
+        this method prompts the user for a file name so we know where to store the information
+        */
+        Console.WriteLine("What file are you working with?");
+        string _file = Console.ReadLine();
+        return _file;
+    }
+}
