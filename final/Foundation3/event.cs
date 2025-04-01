fff17f32ee424e5fc95c266de81ac46ec52b1850
kasonmills <kmillsventura@byui.edu>
2025-03-31 18:05:34 -0600

    prog mad

---
+++ final/Foundation3/event.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace events;
+
+class Event
+{
+
+    protected string EventName;
+    protected string EventDate;
+    protected string EventTime;
+    protected string EventAddress;
+
+    public Event()
+    {
+
+    }
+
+    public void DisplayEventInfo()
+    {
+
+    }
+
+
+}
